4f36434d0245e683766ce67f095b69ef20bac897
jiayong <jiayong711@gmail.com>
2025-04-04 12:08:01 +0800

    Add setup for packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Personal RAG System',
+    version ='0.0.0',
+    author = 'Kang Jia Yong',
+    author_email = 'jiayong711@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
